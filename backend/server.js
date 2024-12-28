d2da8f72309b76e5cde175f0c25ac8168f2fbf75
Raj <rajmalave4@gmail.com>
2024-12-28 22:51:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 initSocket(server);
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://location-tracker-jqm7.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
